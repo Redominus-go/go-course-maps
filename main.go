d262039a798146075ee95ddc0748f289e507652d
Carlos Monroy Fernández <cmonroy@boardfy.com>
2018-09-18 19:09:50 +0200

    Go lang course. Iterating maps S05c50

--- main.go
+++ main.go
@@ -4,13 +4,18 @@ import "fmt"
 
 func main() {
 	//var colors map[string]string
-	/*colors := map[string]string{
+	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#00ff00",
 		"blue":  "#0000ff",
-	}*/
-	colors := make(map[string]string)
-	colors["white"] = "#ffffff"
-	delete(colors, "white")
-	fmt.Println(colors)
+		"white": "#ffffff",
+	}
+	printMap(colors)
+	//	fmt.Println(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Printf("Index: %s,\tValue: %s\n", color, hex)
+	}
 }
